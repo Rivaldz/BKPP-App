9226be8a6ed9fd38c5214b5a8330cb1206bd3568
AdmajaRois <rois.da.silva@gmail.com>
2021-01-09 09:20:13 +0700

    add layout and etc

--- app/Views/admin/layouts/layout.php
+++ app/Views/admin/layouts/layout.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="theme/sbadmin/assets/fontawesome-free/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="theme/sbadmin/css/sb-admin-2.min.css" rel="stylesheet">
 </head>
 <body id="page-top">
     <div id="wrapper">
@@ -19,5 +22,22 @@
     </div>
     <?= $this->include('admin/layouts/footer'); ?>
     
+      <!-- Bootstrap core JavaScript-->
+  <script src="theme/sbadmin/assets/jquery/jquery.min.js"></script>
+  <script src="theme/sbadmin/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="theme/sbadmin/assets/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="theme/sbadmin/js/sb-admin-2.min.js"></script>
+
+  <!-- Page level plugins -->
+  <script src="theme/sbadmin/assets/chart.js/Chart.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="theme/sbadmin/js/demo/chart-area-demo.js"></script>
+  <script src="theme/sbadmin/js/demo/chart-pie-demo.js"></script>
+
 </body>
 </html>
