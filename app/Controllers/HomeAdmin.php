2c2a5704c787ebbb0a29230d8eec3574c31221f5
AdmajaRois <rois.da.silva@gmail.com>
2021-01-13 17:40:54 +0700

    edit bidang done

--- app/Controllers/HomeAdmin.php
+++ app/Controllers/HomeAdmin.php
@@ -33,24 +33,6 @@ class HomeAdmin extends Controller{
 
     }
 
-<<<<<<< HEAD
-    public function editBidang($id){
-        $model = new Bidang_admin_model;
-        $getNamaBidang = $model->getNamaBidang($id)->getRow();
-        if (isset($getNamaBidang)) {
-            $data['bidang'] = $getNamaBidang;
-            $data['title'] = 'Edit '.$getNamaBidang->nama_bidang;
-            view('admin/pages/home_admin',$data);
-
-        }
-        else{
-            '<script>
-                    alert("ID barang '.$id.' Tidak ditemukan");
-                    window.location="'.base_url('barang').'"
-                </script>';
-
-        }
-=======
     public function editBidang($id) {
         
         $this->modelBidang->updateBidang([
@@ -58,6 +40,5 @@ class HomeAdmin extends Controller{
             'nama_bidang'=>$this->request->getVar('nama')
         ]);
         return redirect()->to('/homeadmin');
->>>>>>> a29566423eddaef0cb75b5d7afe4f00a8717df97
     }
 }
