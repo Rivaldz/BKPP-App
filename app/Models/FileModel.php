bf7fa5227cbce2b536210c869229a6e72eb47332
AdmajaRois <rois.da.silva@gmail.com>
2021-01-26 13:43:44 +0700

    file upload belum selesai

--- app/Models/FileModel.php
+++ app/Models/FileModel.php
@@ -4,5 +4,14 @@ namespace  App\Models;
 use CodeIgniter\Model;
 
 class FileModel extends Model {
-    
+    protected $table = 'files_tb';
+    protected $primaryKey = 'id_file';
+    protected $useTimestamps = true;
+    protected $allowedFields = ['name', 'type'];
+    protected $createdField = 'created_at';
+
+    // public function getFile()
+    // {
+        
+    // }
 }
